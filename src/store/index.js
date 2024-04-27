07b8f5e99b16a5fb65aec4ce3f05e08f59d3fe1f
homeniuk <homeniuk@i.ua>
2024-04-27 22:35:39 +0300

    +local storage

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,36 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import listicksReducer from './ListickSlice';
 
+import { persistStore, persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER, } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['listicks'],
+}
+
+const rootReducer = combineReducers({
+    listicks: listicksReducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);    //for 'redux-persist'
+
 const store = configureStore({
-    reducer: {
-        listicks: listicksReducer,
-    },
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],    //for 'redux-persist'
+      },
+    }),
   })
-  
-  export default store
+
+export const persistor = persistStore(store); 
+export default store
